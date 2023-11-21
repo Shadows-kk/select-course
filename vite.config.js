6ea68c24132eb9912f009b9219ac9cdb191bfb7d
Shadows-kk <1213099875@qq.com>
2023-11-21 22:48:08 +0800

    选课

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://5kwhmb.natappfree.cc",
+        target: "http://bjmjxy.natappfree.cc",
         changeOrigin: true,
       },
     },
